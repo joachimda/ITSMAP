7c6e5c77bc097c17d5ef380eb61b34842cd8f65a
Joachim Dam Andersen <joachimdamandersen@gmail.com>
2017-04-19 23:12:47 +0200

    Camera functionality implemented

--- Assignment1-I,Android/I,Android/app/src/main/java/com/example/joachim/iandroid/MainActivity.java
+++ Assignment1-I,Android/I,Android/app/src/main/java/com/example/joachim/iandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.joachim.iandroid;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,16 +10,19 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE_EDIT_ACTIVITY = 100;
+    public static final int REQUEST_CODE_CAPTURE_IMAGE = 101;
 
     private FloatingActionButton floatingActionButtonEdit;
     private EditText editTextName;
     private EditText editTextId;
     private CheckBox checkBoxAndroid;
     private ImageView imageView;
+    private Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +54,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    
 
     @Override
     public void onActivityResult(int request, int result, Intent intent) {
-        switch (result) {
-            case RESULT_OK: {
-                editTextName.setText(intent.getStringExtra("name"));
-                editTextId.setText(intent.getStringExtra("id"));
-                checkBoxAndroid.setChecked(intent.getBooleanExtra(getString(R.string.android_ticked_key), false));
+        if (request == REQUEST_CODE_CAPTURE_IMAGE) {
+            switch (result) {
+                case RESULT_OK: {
+                    if (intent != null) {
+                        Bundle bundle = intent.getExtras();
+                        bitmap = (Bitmap)bundle.get("data");
+                        imageView.setImageBitmap(bitmap);
+                    }
+                }
+                case RESULT_CANCELED: {
+                    //not implemented
+                }
             }
-            case RESULT_CANCELED: {
-                //not implemented
+        }
+
+        if (request == REQUEST_CODE_EDIT_ACTIVITY) {
+            switch (result) {
+                case RESULT_OK: {
+                    editTextName.setText(intent.getStringExtra("name"));
+                    editTextId.setText(intent.getStringExtra("id"));
+                    checkBoxAndroid.setChecked(intent.getBooleanExtra(getString(R.string.android_ticked_key), false));
+                }
+                case RESULT_CANCELED: {
+                    //not implemented
+                }
             }
         }
     }
@@ -71,4 +94,15 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(getString(R.string.android_ticked_key), checkBoxAndroid.isChecked());
         startActivityForResult(intent, REQUEST_CODE_EDIT_ACTIVITY);
     }
+
+    private void startImageCapture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_CODE_CAPTURE_IMAGE);
+        } else {
+            Toast.makeText(this, getString(R.string.image_capture_failed), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
